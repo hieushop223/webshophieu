be5ebdace228f920e7c8df9db550c9c069136dca
Vo Truong Thanh Phat <acccuahieu1919@gmail.com>
2025-12-15 11:01:23 +0700

    update codex2

--- components/alerts/UCAlert.tsx
+++ components/alerts/UCAlert.tsx
@@ -40,7 +40,7 @@ export default function UCAlert({ isOpen, onClose }: UCAlertProps) {
             <div className="flex gap-6 mt-2">
               {/* ZALO */}
               <a
-                href="https://zalo.me/g/cumffz847"
+                href="https://zalo.me/g/ayruet946"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="flex items-center gap-2 px-4 py-2 bg-white/10 hover:bg-white/20 rounded-lg border border-white/30 text-blue-400 hover:text-blue-300 font-bold transition-all"
@@ -53,7 +53,7 @@ export default function UCAlert({ isOpen, onClose }: UCAlertProps) {
 
               {/* Facebook */}
               <a
-                href="https://www.facebook.com/Ilovebapgi/"
+                href="https://www.facebook.com/share/1BuyE1mxLT/?mibextid=wwXIfr"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="flex items-center gap-2 px-4 py-2 bg-white/10 hover:bg-white/20 rounded-lg border border-white/30 text-blue-400 hover:text-blue-300 font-bold transition-all"
@@ -62,21 +62,6 @@ export default function UCAlert({ isOpen, onClose }: UCAlertProps) {
                   <Image src="/images/facebook.svg" alt="Facebook" width={24} height={24} className="brightness-0 invert" />
                 </div>
                 FB
-              </a>
-
-              {/* TikTok */}
-              <a
-                href="https://www.tiktok.com/@emfat_pxt?lang=vi-VN"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="flex items-center gap-2 px-4 py-2 bg-white/10 hover:bg-white/20 rounded-lg border border-white/30 text-white hover:text-gray-300 font-bold transition-all"
-              >
-                <div className="bg-white/20 p-1 rounded">
-                  <svg width="24" height="24" viewBox="0 0 24 24" fill="white">
-                    <path d="M19.59 6.69a4.83 4.83 0 0 1-3.77-4.25V2h-3.45v13.67a2.89 2.89 0 0 1-5.2 1.74 2.89 2.89 0 0 1 2.31-4.64 2.93 2.93 0 0 1 .88.13V9.4a6.84 6.84 0 0 0-1-.05A6.33 6.33 0 0 0 5 20.1a6.34 6.34 0 0 0 10.86-4.43v-7a8.16 8.16 0 0 0 4.77 1.52v-3.4a4.85 4.85 0 0 1-1-.1z"/>
-                  </svg>
-                </div>
-                TikTok
               </a>
             </div>
 
